fca26f9994030180c42267fd42df87976ca55c8b
lucaseuz <lucaseusttaquio@gmail.com>
2026-05-01 11:15:17 -0300

    feat(db): configuração do banco de dados SQLite e modelos do SQLAlchemy

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Boolean, Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from .database import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, nullable=True)
+    completed = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
